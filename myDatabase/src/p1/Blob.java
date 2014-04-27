12309c5f9bd4e0f4b6b78d71b3a64b59af6b561e
wangxu <xwang125@xwang125-Latitude-E5410.(none)>
2014-04-27 15:40:36 -0400

    message

--- myDatabase/src/p1/Blob.java
+++ myDatabase/src/p1/Blob.java
@@ -1,10 +1,14 @@
 package p1;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.Buffer;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -13,54 +17,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Blob {
+	
+
 	public Connection getConnection() {
-		Connection connection = null;
+		Connection connection = null;		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		}catch (ClassNotFoundException e){
 			 System.out.println("No jdbc found");			
 		}
 		try {
-			connection = DriverManager.getConnection("jdbc:mysql://Localhost/myDatabase", "root", "password");
+			connection = DriverManager.getConnection("jdbc:mysql://Localhost/myDatabase", "root", "ps");
 		}catch(Exception e) {
 			System.out.println("Error Occured While connecting " + e);
 		}
 		return connection;
 	}
 	
-	public void select() 
-	{
-		Connection connection = null;
-		PreparedStatement statement = null;
-		//FileInputStream inputStream = null;
-try {
-//			String select = "select * from people;";
-//			connection = getConnection();
-//			statement = connection.createStatement();
-//			ResultSet result = statement.executeQuery(select);
-//			System.out.println(result);
-			//String name = "Kate";
-			String insert = "insert into people values (?, ?)";
-			System.out.println(insert);
-			connection = getConnection();
-			statement = connection.prepareStatement(insert);
-			//String name = "(\"Alex\", 20);";
-			statement.setString(1, "Xu");
-		    statement.setInt(2, 20);
-		    System.out.println(statement);
-			statement.executeUpdate();
-		}catch (SQLException e) {
-			System.out.println("SQLException: " + e);
-		}finally {
-			try {
-				connection.close();
-				statement.close();
-			}catch (SQLException e) {
-				System.out.println("SQLException: " + e);
-			}
-		}
-		
-	}
+
 	public void insertFile() {
 		Connection connection = null;
 		PreparedStatement statement = null;
@@ -68,7 +42,7 @@ try {
 	
 	
 		try {
-			File file = new File("/home/xwang125/sample.txt");
+			File file = new File("/home/xwang125/input.txt");
 			inputStream = new FileInputStream(file);
 			String insert = "insert into myBlob values(1, ?, ?)";
 			connection = getConnection();
@@ -90,9 +64,35 @@ try {
 		}
 	}
 	
-	public static void main(String[] args) throws  SQLException{
+	public void retrieveFile() throws SQLException, IOException {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		FileInputStream inputStream = null;
+		String select = "select pic from myBlob;";
+		connection = getConnection();
+		PreparedStatement st = connection.prepareStatement(select);
+		ResultSet rs = st.executeQuery();
+		String total = "";
+		File output = new File("/home/xwang125/output.txt");
+		FileWriter fw = new FileWriter(output.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		while(rs.next()) {
+			java.sql.Blob myPic = rs.getBlob("pic");
+			java.io.InputStream myInputStream = myPic.getBinaryStream();
+			BufferedReader in = new BufferedReader(new java.io.InputStreamReader(myInputStream));
+			
+			String str;
+			while((str = in.readLine()) != null) {
+				bw.write(str);
+			}
+			myInputStream.close();
+			bw.close();
+		}
+		
+	}	
+		public static void main(String[] args) throws  SQLException, IOException{
 		Blob testBlob = new Blob();
 		System.out.println("Hello");
-		testBlob.select();
+		testBlob.retrieveFile();
 	}
 }
