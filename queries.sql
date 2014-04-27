26725d78251cba3010d03d1fbd2d7c17dc5b04a3
Jeff <jeffs7tao@gmail.com>
2014-04-27 15:38:03 -0400

    update

--- queries.sql
+++ queries.sql
@@ -8,3 +8,16 @@ union
 union
 (select * from quotes where ticker = "ACE" and time >= "2009-01-03" order by time asc limit 1);
 
+#calculate max drop
+select a.ticker, a.adjclose, b.adjclose, (a.adjclose - b.adjclose)/a.adjclose "Drop" from quotes a, quotes b 
+where a.ticker = b.ticker 
+    and a.time < b.time 
+    and a.adjclose > b.adjclose 
+    and a.ticker = "ACE"
+order by (a.adjclose-b.adjclose)/a.adjclose desc limit 5;
+
+#rank portfolio by total return
+select a.adjclose / b.adjclose from
+    (select * from fund, value where fund.name = value.fund order by time desc limit 1) a,
+    (select * from fund, value where fund.name = value.fund order by time asc limit 1) b
+where a. 
